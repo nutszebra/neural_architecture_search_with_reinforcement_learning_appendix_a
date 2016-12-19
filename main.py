efc85069ed82f78f4c337821568fc2c73dd39f87
nutszebra <nutszebra@gmail.com>
2016-12-19 21:04:16 +0900

    main script

---
+++ main.py
@@ -0,0 +1,57 @@
+import sys
+sys.path.append('./trainer')
+import argparse
+import nutszebra_cifar10
+import neural_architecture_search_appendix_a
+import nutszebra_data_augmentation
+import nutszebra_optimizer
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='cifar10')
+    parser.add_argument('--load_model', '-m',
+                        default=None,
+                        help='trained model')
+    parser.add_argument('--load_optimizer', '-o',
+                        default=None,
+                        help='optimizer for trained model')
+    parser.add_argument('--load_log', '-l',
+                        default=None,
+                        help='optimizer for trained model')
+    parser.add_argument('--save_path', '-p',
+                        default='./',
+                        help='model and optimizer will be saved every epoch')
+    parser.add_argument('--epoch', '-e', type=int,
+                        default=200,
+                        help='maximum epoch')
+    parser.add_argument('--batch', '-b', type=int,
+                        default=32,
+                        help='mini batch number')
+    parser.add_argument('--gpu', '-g', type=int,
+                        default=-1,
+                        help='-1 means cpu mode, put gpu id here')
+    parser.add_argument('--start_epoch', '-s', type=int,
+                        default=1,
+                        help='start from this epoch')
+    parser.add_argument('--train_batch_divide', '-trb', type=int,
+                        default=4,
+                        help='divid batch number by this')
+    parser.add_argument('--test_batch_divide', '-teb', type=int,
+                        default=4,
+                        help='divid batch number by this')
+    parser.add_argument('--lr', '-lr', type=float,
+                        default=0.1,
+                        help='leraning rate')
+
+    args = parser.parse_args().__dict__
+    lr = args.pop('lr')
+
+    print('generating model')
+    model = neural_architecture_search_appendix_a.AppendixA(10)
+    print('Done')
+    optimizer = nutszebra_optimizer.OptimizerAppendixA(model, lr=lr)
+    args['model'] = model
+    args['optimizer'] = optimizer
+    args['da'] = nutszebra_data_augmentation.DataAugmentationCifar10NormalizeSmall
+    main = nutszebra_cifar10.TrainCifar10(**args)
+    main.run()
